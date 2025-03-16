aa9fd524311ea781645db8c8dfefb2b522f4cc2b
amiinmuse <amiinmuse2025@u.northwestern.edu>
2025-03-16 18:41:39 -0500

    latest changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box, Typography, IconButton } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 // import InstagramIcon from "@mui/icons-material/Instagram";
-import TwitterIcon from "@mui/icons-material/Twitter";
+// import TwitterIcon from "@mui/icons-material/Twitter";
 // import { useEffect, useState } from "react";
 
 const Footer = () => {
